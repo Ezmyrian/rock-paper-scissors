a297f59d3b6b3e194eb718db5bbba8ed23affefb
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2025-03-30 12:13:51 -0500

    Remove psuedocode comments and adjust variable indentation

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-/*
-Create function getComputerChoice that randomly picks a number between 0 and 2
-    Initialize variable randomNum of type number and assign a value
-        between 0 and 2.
-    Create logic block to assign rock, paper, or scissors based on 
-        randomNum and return the value.
-*/
-
 function getComputerChoice() {
-    let randomNum = Math.floor(Math.random()*3);
+    let randomNum = Math.floor(Math.random()*3);  //picks a random number between 0 and 2
     switch (randomNum) {
         case 0:
             return "rock";
@@ -18,34 +10,14 @@ function getComputerChoice() {
     }
 }
 
-/*
-Create function getHumanChoice that prompts user for choice of rock, paper, or scissors.
-Return value of users choice.
-*/
-
 function getHumanChoice() {
     let humanChoice = prompt("Rock, paper, or scissors?");
     return humanChoice;
 }
 
-/*
-Move playRound function and score variables into a playGame function 
-and call playRound five times
-*/
 function playGame() {
-
-//Create two variables in global scope to track human and computer scores
-
-let humanScore = 0;
-let computerScore = 0;
-
-/*
-Create function playRound which accepts two parameters, computerChoice and humanChoice.
-    Make humanChoice case insensitive
-    Compare choices to determine the winner
-    Console log the winner
-    Increment human or computer score as appropriate
-*/
+    let humanScore = 0;
+    let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
