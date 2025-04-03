315226d1be63d7618036a294e4e95dabeeeffbed
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2025-04-03 16:11:44 -0500

    Add event listeners and score tracking
    Added buttons to make play easier for player.
    Move computer choice function inside playRound so it would update for
    each round.
    Added divs for score tracking so player would know current score.

--- script.js
+++ script.js
@@ -1,70 +1,99 @@
-function getComputerChoice() {
-    let randomNum = Math.floor(Math.random()*3);  //picks a random number between 0 and 2
-    switch (randomNum) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2: 
-            return "scissors";
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice) {
+    let display = document.querySelector(".results");
+    let currentScore = document.querySelector(".current-score");
+    let computerChoice = getComputerChoice();
+    function getComputerChoice() {
+        let randomNum = Math.floor(Math.random()*3);  //picks a random number between 0 and 2
+        switch (randomNum) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2: 
+                return "scissors";
+        }
+    }
+
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                display.textContent = "Tie";
+                break;
+            case "paper":
+                computerScore += 1;
+                display.textContent = "You lose";
+                break;
+            case "scissors":
+                humanScore += 1;
+                display.textContent = "You win!";
+                break;
+        }
+    }
+    else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                humanScore += 1;
+                display.textContent = "You win!";
+                break;
+            case "paper":
+                display.textContent = "Tie";
+                break;
+            case "scissors":
+                computerScore += 1;
+                display.textContent = "You lose";
+                break;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                computerScore += 1;
+                display.textContent = "You lose";
+                break;
+            case "paper":
+                humanScore += 1;
+                display.textContent = "You win!";
+                break;
+            case "scissors":
+                display.textContent = "Tie";
+                break;
+        }
+    }
+    function Score () {
+        currentScore.textContent = `Computer Score: ${computerScore} 
+                                    Player Score: ${humanScore}`;
+    }
+    Score();
+    if (computerScore == 5) {
+        display.textContent = "Game over, Skynet has taken the Earth."
+        computerScore = 0;
+        humanScore = 0;
+        Score();
+    }
+    else if (humanScore == 5) {
+        display.textContent = "You have defeated Skynet!"
+        computerScore = 0;
+        humanScore = 0;
+        Score();
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, or scissors?");
-    return humanChoice;
-}
+const buttons = document.querySelector(".buttons");
+buttons.addEventListener('click', (e) => {
+    let target = e.target;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        if (humanChoice === "rock") {
-            switch (computerChoice) {
-                case "rock":
-                    return "It's a tie";
-                case "paper":
-                    computerScore += 1;
-                    return "You lose."
-                case "scissors":
-                    humanScore += 1;
-                    return "Tou win!"
-            }
-        }
-        else if (humanChoice === "paper") {
-            switch (computerChoice) {
-                case "rock":
-                    humanScore += 1;
-                    return "Tou win!"
-                case "paper":
-                    return "It's a tie";
-                case "scissors":
-                    computerScore += 1;
-                    return "You lose."
-            }
-        }
-        else if (humanChoice === "scissors") {
-            switch (computerChoice) {
-                case "rock":
-                    computerScore += 1;
-                    return "You lose."
-                case "paper":
-                    humanScore += 1;
-                    return "Tou win!"
-                case "scissors":
-                    return "It's a tie";
-            }
-        }
-        else {
-            return console.log("Not a valid player entry");
-        }
+    switch (target.id) {
+        case 'rock':
+            playRound('rock')
+            break;
+        case 'paper':
+            playRound('paper');
+            break;
+        case 'scissors':
+            playRound('scissors')
+            break;
     }
-    for (i = 1; i < 6; i++) {
-        playRound(getHumanChoice(), getComputerChoice())
-        console.log(`player score is ${humanScore}`, `computer score is ${computerScore}`)
-    }
-}
-
-playGame()
+});
